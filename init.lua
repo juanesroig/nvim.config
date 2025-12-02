4dc3904d568525f0d205752ca43001edc32f5a87
Juanes Roig <juan.roig@taptapdigital.com>
2025-12-02 16:48:06 +0100

    Change live grep shortcut

--- init.lua
+++ init.lua
@@ -435,8 +435,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      -- vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       -- vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -688,6 +688,49 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                logLevel = 'Trace',
+                disableLanguageServices = false,
+                disableOrganizeImports = false,
+                disableTaggedHints = false,
+                diagnosticMode = "openFilesOnly",
+                stubPath = './stubs',
+                -- diagnosticMode = "workspace",
+                inlayHints = {
+                  variableTypes = true,
+                  callArgumentNames = true,
+                  functionReturnTypes = true,
+                  genericTypes = true,
+                },
+                useTypingExtensions = false,
+                fileEnumerationTimeout = 10, -- in seconds
+                typeCheckingMode = 'basic',
+                useLibraryCodeForTypes = true,
+                diagnosticSeverityOverrides = {
+                  reportPrivateImportUsage = false,
+                  reportRedeclaration = false,
+                  -- reportMissingParameterType = false,
+                  -- reportUnknownParameterType = false,
+                  -- reportUnknownVariableType = false,
+                  -- reportUnknownMemberType = false,
+                  -- reportUnknownArgumentType = false,
+                  -- reportUnusedCallResult = false,
+                  -- reportAny = false,
+                  -- reportMissingTypeStubs = false,
+                  -- reportImplicitStringConcatenation = false,
+                  -- reportUnannotatedClassAttribute = false,
+                  -- reportImplicitOverride = false,
+                  -- reportIncompatibleMethodOverride = false,
+                  -- reportUnknownLambdaType = false,
+                  -- reportExplicitAny = false,
+                },
+              }
+            }
+          }
+        },
 
         lua_ls = {
           -- cmd = { ... },
